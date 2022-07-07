e2eccd653850b12ff8b8321a4b1cbee0ccf368f6
Paul Hankins <Paulhankins99@gmail.com>
2022-07-07 13:13:45 -0500

    comments

--- Spark Student Analysis.py
+++ Spark Student Analysis.py
@@ -3,19 +3,19 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setAppName("Mini Project")
 sc = SparkContext.getOrCreate(conf=conf)
 
-# COMMAND ----------
+# Load Data
 
 rdd = sc.textFile('/FileStore/tables/StudentData.csv')
 headers = rdd.first()
 rdd = rdd.filter(lambda x: x!=headers)
 rdd = rdd.map(lambda x: x.split(','))
 
-# COMMAND ----------
+# Total Students
 
 # 1
 rdd.count()
 
-# COMMAND ----------
+# Total Marks by Male and Female Students
 
 # 2
 rdd2 = rdd
@@ -23,7 +23,7 @@ rdd2 = rdd2.map(lambda x: (x[1], int(x[5])))
 rdd2 = rdd2.reduceByKey(lambda x,y : x+y)
 rdd2.collect()
 
-# COMMAND ----------
+# Total Passed and Failed Students
 
 #3
 rdd3 = rdd
@@ -35,21 +35,21 @@ passed2 = rdd3.filter(lambda x: int(x[5]) > 50).count()
 failed2 = rdd.count() - passed2
 print(passed2,failed2)
 
-# COMMAND ----------
+# Total Enrollments per Course
 
 # 4
 rdd4 = rdd
 rdd4 = rdd4.map(lambda x: (x[3],1))
 rdd4.reduceByKey(lambda x,y: x+y).collect()
 
-# COMMAND ----------
+# Total Marks per Course
 
 # 5
 rdd5 = rdd
 rdd5 = rdd5.map(lambda x: (x[3], int(x[5])))
 rdd5.reduceByKey(lambda x,y: x+y).collect()
 
-# COMMAND ----------
+# Average Marks per Course
 
 # 6
 rdd6 = rdd
@@ -60,7 +60,7 @@ rdd6.map(lambda x: (x[0], (x[1][0] / x[1][1]))).collect()
 rdd6.mapValues(lambda x: (x[0] / x[1])).collect()
 
 
-# COMMAND ----------
+# Finding Minimum and Maximum Marks
 
 # 7
 rdd7 = rdd
@@ -68,7 +68,7 @@ rdd7 = rdd7.map(lambda x: (x[3], int(x[5])))
 print(rdd7.reduceByKey(lambda x,y: x if x > y else y).collect())
 print(rdd7.reduceByKey(lambda x,y: x if x < y else y).collect())
 
-# COMMAND ----------
+# Average Age of Male and Female Students
 
 # 8
 rdd8 = rdd
